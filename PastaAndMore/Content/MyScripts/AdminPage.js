ecb121101305f4c0675e22bb8f9c2c7def413dd6
leomax2016 <leomax2016@mail.ru>
2018-12-03 23:25:37 +0400

    tabs

--- PastaAndMore/Content/MyScripts/AdminPage.js
+++ PastaAndMore/Content/MyScripts/AdminPage.js
@@ -1,27 +1,27 @@
 ﻿$(() => {
-    $('#Add').on('click', () => {
-        var obj = { product: $('#productName').val(), desc: $('#productDesc').val(), cat: $('#Cats').val()}
-        console.log(obj);
-        var jsonObj = JSON.stringify(obj);
-        console.log(jsonObj);
+	$('#Add').on('click', () => {
+		var obj = { product: $('#productName').val(), desc: $('#productDesc').val(), cat: $('#Cats').val()}
+		console.log(obj);
+		var jsonObj = JSON.stringify(obj);
+		console.log(jsonObj);
 
-        $.ajax({
-            type: "POST",
-            url: 'AddProduct',
-            data: jsonObj,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: successFunc,
-            error: errorFunc
-        });
+		$.ajax({
+			type: "POST",
+			url: 'AddProduct',
+			data: jsonObj,
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: successFunc,
+			error: errorFunc
+		});
 
-        function successFunc(data, status) {
-            alert(data);
-        }
+		function successFunc(data, status) {
+			alert(data);
+		}
 
-        function errorFunc() {
-            alert('error');
-        }
-    })
+		function errorFunc() {
+			alert('error');
+		}
+	})
 })
 
